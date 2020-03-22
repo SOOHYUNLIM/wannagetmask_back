d31c904d0503f768adeb58411b801cab50f0c02f
SOOHYUNLIM <sh_7682@naver.com>
2020-03-22 20:09:02 +0900

    자동구매 DB 저장 및 수정

--- src/main/java/com/wannagetmask/controller/MaskRestController.java
+++ src/main/java/com/wannagetmask/controller/MaskRestController.java
@@ -41,49 +41,67 @@ public class MaskRestController {
     @Autowired
     private List<Target> targetList;
 
-        // 옵션 반환
-        @GetMapping("/intoMarket/{url}")
-        public ResponseEntity<Map<String, List<Option>>> intoMarket(@PathVariable String url) {
-            String rink = URLDecoder.decode(url);
-            Map<String, List<Option>> result = null;
-            HttpStatus status = HttpStatus.OK;
-            try{
-                result = seleniumUtil.intoNaverShopping(rink);
-            } catch (Exception e) {
-                status = HttpStatus.RESET_CONTENT;
-            }
-            return new ResponseEntity<>(result, status);
+    // 자동구매 대상 옵션 반환
+    @GetMapping("/intoMarket/{url}")
+    public ResponseEntity<Map<String, List<Option>>> intoMarket(@PathVariable String url) {
+        String rink = URLDecoder.decode(url);
+        Map<String, List<Option>> result = null;
+        HttpStatus status = HttpStatus.OK;
+        try {
+            result = seleniumUtil.intoNaverShopping(rink);
+        } catch (Exception e) {
+            status = HttpStatus.RESET_CONTENT;
+        }
+        return new ResponseEntity<>(result, status);
     }
 
+    //자동구매 등록
     @PostMapping("/registerMarket")
     public ResponseEntity<Boolean> registerMarket(@RequestBody Market market) {
         Boolean result = true;
         //DB에 넣을 것
-        try{
+        try {
             marketRepository.insert(market);
         } catch (Exception e) {
-        //이미 등록되었을 시(PK 중복 시)
+            //이미 등록되었을 시(PK 중복 시)
             result = false;
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    //자동구매 수정(옵션, ON/OFF)
+    @PutMapping("/updateMarket")
+    public ResponseEntity<CustomMessage> updateMarket(@RequestBody Market market) {
+        marketRepository.save(market);
+        return null;
+    }
+
+    //자동구매 목록
     @GetMapping("/getMarkets/{page}")
-    public ResponseEntity<Page> getMarkets(@PathVariable Integer page) {
+    public ResponseEntity<Page<Market>> getMarkets(@PathVariable Integer page) {
         Page<Market> result = marketRepository.findAll(PageRequest.of(page, 10));
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+
+    //계정 등록
     @PostMapping("/registerAccount")
     public ResponseEntity<CustomMessage> registerAccount(@RequestBody Account account) {
         CustomMessage result = seleniumUtil.loginNaver(account.getId(), account.getPw()) ? CustomMessage.SUCCESS : CustomMessage.FAIL;
-        if(result.equals(CustomMessage.SUCCESS)) {
-            try{
+        if (result.equals(CustomMessage.SUCCESS)) {
+            try {
                 accountRepository.insert(account);
             } catch (Exception e) {
                 result = CustomMessage.OVERLAP;
             }
         }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    //계정 목록
+    @GetMapping("/getAccounts/{page}")
+    public ResponseEntity<Page<Account>> getAccounts(@PathVariable Integer page) {
+        Page<Account> result = accountRepository.findAll(PageRequest.of(page, 10));
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
@@ -101,9 +119,8 @@ public class MaskRestController {
 
     @GetMapping("/getTargetList")
     public ResponseEntity<List<Target>> targetListUp() {
-        return new ResponseEntity<>(targetRepository.findAll(),HttpStatus.OK);
+        return new ResponseEntity<>(targetRepository.findAll(), HttpStatus.OK);
     }
-
 
 
 }
