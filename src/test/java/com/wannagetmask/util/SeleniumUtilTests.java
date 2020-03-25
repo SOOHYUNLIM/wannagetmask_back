bc73470df6d47c472fa783e41811e8feb0722323
SOOHYUNLIM <sh_7682@naver.com>
2020-03-25 14:25:16 +0900

    Update SeleniumUtilTests.java

--- src/test/java/com/wannagetmask/util/SeleniumUtilTests.java
+++ src/test/java/com/wannagetmask/util/SeleniumUtilTests.java
@@ -8,13 +8,13 @@ public class SeleniumUtilTests {
 
     @Test
     public void sendMsgTest() {
-        seleniumUtil.loginKakaoAdmin("tjsvndrlskfk@naver.com", "tngus645312");
+        seleniumUtil.loginKakaoAdmin("", "");
         seleniumUtil.sendMessage("쿠팡", "https://www.coupang.com/vp/products/71343011?itemId=238087169&vendorItemId=3584992631&q=%ED%83%90%EC%82%AC+%EB%A7%88%EC%8A%A4%ED%81%AC&itemsCount=30&searchId=6c3f8e43e15f4b9484491833a8b04d39&rank=0&isAddedCart=");
     }
 
     @Test
     public void loginTest() {
-        seleniumUtil.loginNaver("tngus1337", "rlxhdqks001!");
-        seleniumUtil.loginKakaoAdmin("tjsvndrlskfk@naver.com", "tngus645312");
+        seleniumUtil.loginNaver("", "");
+        seleniumUtil.loginKakaoAdmin("", "");
     }
 }
